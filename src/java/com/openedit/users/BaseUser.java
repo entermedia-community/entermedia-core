4145f34e6f155af5a9ca4eae57c45cdde5c2f898
Ian <ian@ijsolutions.ca>
2012-05-08 16:14:49 -0400

    Enabled flag display bug.

--- src/java/com/openedit/users/BaseUser.java
+++ src/java/com/openedit/users/BaseUser.java
@@ -467,6 +467,11 @@ public class BaseUser extends FileSystemObject implements User, Comparable
 
 	public void setEnabled(boolean inEnabled) {
 		fieldEnabled = inEnabled;
+		if(inEnabled){
+		setProperty("enabled", "true");
+		}else{
+			setProperty("enabled", "false");
+		}
 	}
 	
 	public Map listAllProperties()
