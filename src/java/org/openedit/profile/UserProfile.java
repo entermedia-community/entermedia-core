c6369598db7faa77f514d01026e63886940e720e
Ian <ian@ijsolutions.ca>
2012-10-02 09:31:47 -0400

    Fixed display issue.  Switched back to proper code profile

--- src/java/org/openedit/profile/UserProfile.java
+++ src/java/org/openedit/profile/UserProfile.java
@@ -19,7 +19,8 @@ import com.openedit.hittracker.HitTracker;
 import com.openedit.hittracker.SearchQuery;
 import com.openedit.users.User;
 
-public class UserProfile extends ElementData {
+public class UserProfile extends ElementData
+{
 	protected String fieldCatalogId;
 	protected SearcherManager fieldSearcherManager;
 	protected Data fieldSettingsGroup;
@@ -31,70 +32,79 @@ public class UserProfile extends ElementData {
 
 	protected User fieldUser;
 
-	public User getUser() {
+	public User getUser()
+	{
 		return fieldUser;
 	}
 
-	public void setUser(User inUser) {
+	public void setUser(User inUser)
+	{
 		fieldUser = inUser;
 	}
 
-	
-	
 	private static final Log log = LogFactory.getLog(UserProfile.class);
 
-	
 	public UserProfile()
 	{
-		if( 1 > 32)
+		if (1 > 32)
 		{
-			
 
 		}
 	}
 
-	public String getCatalogId() {
+	public String getCatalogId()
+	{
 		return fieldCatalogId;
 	}
 
-	public void setCatalogId(String catalogId) {
+	public void setCatalogId(String catalogId)
+	{
 		fieldCatalogId = catalogId;
 	}
 
-	public SearcherManager getSearcherManager() {
+	public SearcherManager getSearcherManager()
+	{
 		return fieldSearcherManager;
 	}
 
-	public void setSearcherManager(SearcherManager searcherManager) {
+	public void setSearcherManager(SearcherManager searcherManager)
+	{
 		fieldSearcherManager = searcherManager;
 	}
 
-	public String getUserId() {
+	public String getUserId()
+	{
 		return get("userid");
 	}
 
-	public boolean isEnabled(String inPreference) {
+	public boolean isEnabled(String inPreference)
+	{
 		String val = getValue(inPreference);
 		return Boolean.parseBoolean(val);
 	}
 
-	public String getValue(String inPreference) {
-		if (inPreference == null) {
+	public String getValue(String inPreference)
+	{
+		if (inPreference == null)
+		{
 			return null;
 		}
 		String val = get(inPreference);
 
-		if (val == null && getUser() != null) {
-			val = getUser().get(inPreference);
+		if (val == null && getSettingsGroup() != null)
+		{
+			val = getSettingsGroup().get(inPreference);
 		}
-		if (val == null && getSettingsGroup() != null) {
-			return getSettingsGroup().get(inPreference);
+		if (val == null && getUser() != null)
+		{
+			val = getUser().get(inPreference);
 		}
 
 		return val;
 	}
 
-	public Collection getValues(String inPreference) {
+	public Collection getValues(String inPreference)
+	{
 		String val = getValue(inPreference);
 
 		if (val == null)
@@ -107,32 +117,42 @@ public class UserProfile extends ElementData {
 		return collection;
 	}
 
-	public String replaceUserVariable(String inValue) {
-		if (inValue == null) {
+	public String replaceUserVariable(String inValue)
+	{
+		if (inValue == null)
+		{
 			return inValue;
 		}
 		String value = inValue;
 		int start = 0;
-		while ((start = value.indexOf("$[", start)) != -1) {
+		while ((start = value.indexOf("$[", start)) != -1)
+		{
 			int end = value.indexOf("]", start);
-			if (end != -1) {
+			if (end != -1)
+			{
 				String key = value.substring(start + 2, end);
 				String variable = getValue(key); // check for property
 
-				if (variable != null) {
-					value = value.substring(0, start) + variable
-							+ value.substring(end + 1);
-					if (variable.length() <= end) {
+				if (variable != null)
+				{
+					value = value.substring(0, start) + variable + value.substring(end + 1);
+					if (variable.length() <= end)
+					{
 						start = end - variable.length();
-					} else {
+					}
+					else
+					{
 						start = variable.length();
 					}
-				} else {
+				}
+				else
+				{
 					start = end;
 				}
 			}
 		}
-		if (start > 0 && inValue.equals(value)) {
+		if (start > 0 && inValue.equals(value))
+		{
 			value = value.replace('[', '{');
 			value = value.replace(']', ']');
 		}
@@ -140,104 +160,168 @@ public class UserProfile extends ElementData {
 		return value;
 	}
 
-	public Data getSettingsGroup() {
-		if (fieldSettingsGroup == null) {
+	public Data getSettingsGroup()
+	{
+		if (fieldSettingsGroup == null)
+		{
 			String groupid = get("settingsgroup");
-			if (groupid == null) {
+			if (groupid == null)
+			{
 				groupid = "guest";
 			}
 			Searcher settingsGroupSearcher = getSearcherManager().getSearcher(getCatalogId(), "settingsgroup");
-			fieldSettingsGroup = (Data)settingsGroupSearcher.searchById(groupid);
-			if( fieldSettingsGroup == null && log.isDebugEnabled() )
+			fieldSettingsGroup = (Data) settingsGroupSearcher.searchById(groupid);
+			if (fieldSettingsGroup == null && log.isDebugEnabled())
 			{
-				log.debug("No settings group defined" );
+				log.debug("No settings group defined");
 			}
 		}
 		return fieldSettingsGroup;
 	}
 
-	public void setSettingsGroup(String inSettingsGroupId) {
+	public void setSettingsGroup(String inSettingsGroupId)
+	{
 		setProperty("settingsgroupid", inSettingsGroupId);
 		fieldSettingsGroup = null;
 	}
 
-	public void save(User inUser) {
-		Searcher searcher = getSearcherManager().getSearcher(getCatalogId(),
-				"userprofile");
+	public void save(User inUser)
+	{
+		Searcher searcher = getSearcherManager().getSearcher(getCatalogId(), "userprofile");
 		searcher.saveData(this, inUser);
 	}
 
-	public void setValuesFromDetails(String inKey,
-			Collection<PropertyDetail> inValues) {
+	public void setValuesFromDetails(String inKey, Collection<PropertyDetail> inValues)
+	{
 		StringBuffer values = new StringBuffer();
-		for (Iterator iterator = inValues.iterator(); iterator.hasNext();) {
+		for (Iterator iterator = inValues.iterator(); iterator.hasNext();)
+		{
 			PropertyDetail detail = (PropertyDetail) iterator.next();
 			values.append(detail.getId());
-			if (iterator.hasNext()) {
+			if (iterator.hasNext())
+			{
 				values.append(" ");
 			}
 		}
 		setProperty(inKey, values.toString());
 	}
 
-	public void setValues(String inKey, Collection<String> inValues) {
+	public void setValues(String inKey, Collection<String> inValues)
+	{
 		StringBuffer values = new StringBuffer();
-		for (Iterator iterator = inValues.iterator(); iterator.hasNext();) {
+		for (Iterator iterator = inValues.iterator(); iterator.hasNext();)
+		{
 			String detail = (String) iterator.next();
 			values.append(detail);
-			if (iterator.hasNext()) {
+			if (iterator.hasNext())
+			{
 				values.append(" ");
 			}
 		}
 		setProperty(inKey, values.toString());
 	}
 
-	public void addValue(String inKey, String string) {
+	public void addValue(String inKey, String string)
+	{
 		String current = get(inKey);
-		if (string != null) {
+		if (string != null)
+		{
 			string = string.trim();
 		}
-		if (current == null || current.length() == 0) {
+		if (current == null || current.length() == 0)
+		{
 			setProperty(inKey, string);
-		} else {
+		}
+		else
+		{
 			setProperty(inKey, current + " " + string);
 		}
 	}
 
-	public void removeValue(String inKey, String string) {
+	public void removeValue(String inKey, String string)
+	{
 		Collection collection = getValues(inKey);
-		if (collection != null) {
+		if (collection != null)
+		{
 			Collection keys = new ArrayList(collection);
 			keys.remove(string);
 			setValues(inKey, keys);
 		}
 	}
 
-	public String toString() {
-		return "User Profile";
+	public String toString()
+	{
+
+		return getScreenName();
+		
+		
+		
+
+	}
+	public String getScreenName()
+	{
+		String sn = (String)get("screenname");
+		
+		if (sn == null)
+		{
+			return getShortDescription();
+		}
+		return sn;
+	}
+	
+	public String getShortDescription()
+	{
+		StringBuffer out = new StringBuffer();
+		if ( get("firstname") != null)
+		{
+			out.append( get("firstname") );
+			out.append(" ");
+		}
+		if (  get("lastname") != null)
+		{
+			out.append(get("lastname"));
+		}
+		if( out.length() == 0)
+		{
+			if( get("email") != null && Character.isDigit(getUserId().charAt(0) ) )
+			{
+				out.append( get("email") );
+			}
+			else
+			{
+				out.append( getUserId());
+			}
+		}
+		return out.toString();
 	}
 
-	public HitTracker getCatalogs() {
+	public HitTracker getCatalogs()
+	{
 		return fieldCatalogs;
 	}
 
-	public void setCatalogs(HitTracker inCatalogs) {
+	public void setCatalogs(HitTracker inCatalogs)
+	{
 		fieldCatalogs = inCatalogs;
 	}
 
-	public XmlArchive getXmlArchive() {
+	public XmlArchive getXmlArchive()
+	{
 		return fieldXmlArchive;
 	}
 
-	public void setXmlArchive(XmlArchive inXmlArchive) {
+	public void setXmlArchive(XmlArchive inXmlArchive)
+	{
 		fieldXmlArchive = inXmlArchive;
 	}
 
-	public Map getResultViews() {
+	public Map getResultViews()
+	{
 		return fieldResultViews;
 	}
 
-	public void setResultViews(Map inResultViews) {
+	public void setResultViews(Map inResultViews)
+	{
 		fieldResultViews = inResultViews;
 	}
 
@@ -253,88 +337,107 @@ public class UserProfile extends ElementData {
 	// return null;
 	// }
 
-	public void setResultViewPreference(String inView, String inPreference) {
+	public void setResultViewPreference(String inView, String inPreference)
+	{
 		setProperty(inView, inPreference);
 	}
 
-	public int getHitsPerPageForSearchType(String inResultsView)
-			throws Exception {
+	public int getHitsPerPageForSearchType(String inResultsView) throws Exception
+	{
 		String view = inResultsView + "hitsperpage";
 		String value = getValue(view);
-		if (value == null) {
+		if (value == null)
+		{
 			return 15;
 		}
 		return Integer.parseInt(value);
 	}
 
-	public void setHitsPerPageForSearchType(String inResultsView, int inHits) {
+	public void setHitsPerPageForSearchType(String inResultsView, int inHits)
+	{
 		setProperty(inResultsView + "hitsperpage", String.valueOf(inHits));
 	}
 
-	public void setSortForSearchType(String inResultsView, String inSort) {
+	public void setSortForSearchType(String inResultsView, String inSort)
+	{
 		setProperty(inResultsView + "sort", inSort);
 	}
 
-	public String getSortForSearchType(String inResultsType) {
+	public String getSortForSearchType(String inResultsType)
+	{
 		String value = getValue(inResultsType + "sort");
 		return value;
 	}
 
-	public String getViewForResultType(String inCustomView, String inResultsView) {
-		if (inCustomView != null) {
+	public String getViewForResultType(String inCustomView, String inResultsView)
+	{
+		if (inCustomView != null)
+		{
 			return inCustomView;
 		}
 		String view = getViewForResultType(inResultsView);
-		if (view == null) {
+		if (view == null)
+		{
 			view = "default";
 		}
 		return view;
 	}
 
-	public String getViewForResultType(String inResultsView) {
+	public String getViewForResultType(String inResultsView)
+	{
 		String view = getValue(inResultsView);
 		return view;
 	}
 
-	public Data getLastCatalog() {
+	public Data getLastCatalog()
+	{
 		String catid = get("lastcatalog");
-		for (Iterator iterator = getCatalogs().iterator(); iterator.hasNext();) {
+		for (Iterator iterator = getCatalogs().iterator(); iterator.hasNext();)
+		{
 			Data cat = (Data) iterator.next();
-			if (catid == null || cat.getId().equals(catid)) {
+			if (catid == null || cat.getId().equals(catid))
+			{
 				return cat;
 			}
 		}
-		if (getCatalogs().size() > 0) {
+		if (getCatalogs().size() > 0)
+		{
 			return (Data) getCatalogs().iterator().next();
 		}
 		return null;
 	}
 
-	public HitTracker getUploadCatalogs() {
+	public HitTracker getUploadCatalogs()
+	{
 		return fieldUploadCatalogs;
 	}
 
-	public void setUploadCatalogs(HitTracker inUploadCatalogs) {
+	public void setUploadCatalogs(HitTracker inUploadCatalogs)
+	{
 		fieldUploadCatalogs = inUploadCatalogs;
 	}
 
-	public Collection getCombinedLibraries() {
+	public Collection getCombinedLibraries()
+	{
 		return fieldCombinedLibraries;
 	}
 
-	public void setCombinedLibraries(Collection inCombinedLibraries) {
+	public void setCombinedLibraries(Collection inCombinedLibraries)
+	{
 		fieldCombinedLibraries = inCombinedLibraries;
 	}
 
-	public Data getDefaultViewForModule(String inModuleId) {
-		Searcher viewSearcher = getSearcherManager().getSearcher(
-				getCatalogId(), "view");
+	public Data getDefaultViewForModule(String inModuleId)
+	{
+		Searcher viewSearcher = getSearcherManager().getSearcher(getCatalogId(), "view");
 		SearchQuery q = viewSearcher.createSearchQuery();
 		q.addMatches("module", inModuleId);
 		q.addMatches("systemdefined", "false");
 		HitTracker row = (HitTracker) viewSearcher.search(q);
-		if (row.size() > 0) {
-			if (row != null) {
+		if (row.size() > 0)
+		{
+			if (row != null)
+			{
 				return row.get(0);
 			}
 		}
@@ -342,38 +445,60 @@ public class UserProfile extends ElementData {
 	}
 
 	@Override
-	public String getSourcePath() {
+	public String getSourcePath()
+	{
 
-		if (fieldSourcePath == null) {
-			if (getUser() != null) {
+		if (fieldSourcePath == null)
+		{
+			if (getUser() != null)
+			{
 				return getUser().getId();
 			}
-			if (getUserId() != null) {
+			if (getUserId() != null)
+			{
 				return getUserId();
 			}
 
-		} else {
+		}
+		else
+		{
 			return fieldSourcePath;
 		}
 		return super.getSourcePath();
 	}
 
-	public void setProperty(String inId, String inValue) {
-		if (getUser() != null) {
-			if ("firstname".equals(inId)) {
+	public void setProperty(String inId, String inValue)
+	{
+		if (getUser() != null)
+		{
+			if ("firstname".equalsIgnoreCase(inId))
+			{
 				getUser().setFirstName(inValue);
 			}
-			if ("lastname".equals(inId)) {
+			if ("lastname".equalsIgnoreCase(inId))
+			{
 				getUser().setLastName(inValue);
 			}
-			if ("email".equals(inId)) {
+			if ("email".equalsIgnoreCase(inId))
+			{
 				getUser().setEmail(inValue);
 			}
-			if ("password".equals(inId)) {
+			if ("password".equalsIgnoreCase(inId))
+			{
 				getUser().setPassword(inValue);
 			}
 		}
 		// TODO Auto-generated method stub
 		super.setProperty(inId, inValue);
+	}
+
+@Override
+public String getName()
+{
+	return toString();
+}	
+	
+	public String getText(){
+		return toString();
 	}
 }
