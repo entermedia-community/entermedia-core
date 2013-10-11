691bc516051c1fa5f616442c9eb506cc7c49efdf
smbest <smbest@hotmail.ca>
2013-10-11 15:07:54 -0400

    Replacer now can look for more than one variable to replace on, each of which is delimited by a "||", eg. ${asset.assettitle||asset.name}

--- src/java/com/openedit/util/Replacer.java
+++ src/java/com/openedit/util/Replacer.java
@@ -1,6 +1,8 @@
 package com.openedit.util;
 
+import java.util.ArrayList;
 import java.util.Map;
+import java.util.StringTokenizer;
 
 import org.openedit.Data;
 import org.openedit.data.SearcherManager;
@@ -66,33 +68,44 @@ public class Replacer
 			{
 				break;
 			}
+			
 			String key = inCode.substring(start+2,end);
-			Object variable = inValues.get(key); //check for property
-			if( variable == null )
+			Object variable = null;
+			ArrayList<String> values = findKeys(key,"||");
+			
+			for(String value:values)
 			{
-				//TODO: Loop over each of the dots to find the final object
-				int dot = key.indexOf('.');
-				if( dot > 0)
+				variable = inValues.get(value); //check for property
+				if( variable == null )
 				{
-					String objectname = key.substring(0,dot);
-					
-					Object object = inValues.get(objectname);  //${division.folder}
-					if( object instanceof String )
+					//TODO: Loop over each of the dots to find the final object
+					int dot = value.indexOf('.');
+					if( dot > 0)
 					{
-						object = getData(objectname,(String)object); //division
-						if(isAlwaysReplace() && object == null)
+						String objectname = value.substring(0,dot);
+						
+						Object object = inValues.get(objectname);  //${division.folder}
+						if( object instanceof String )
 						{
-							variable="";
+							object = getData(objectname,(String)object); //division
+							if(isAlwaysReplace() && object == null)
+							{
+								variable="";
+							}
+						}
+						if(object instanceof Data)
+						{
+							Data data = (Data)object;
+							String method = value.substring(dot+1);
+							variable = data.get(method);
 						}
 					}
-					if(object instanceof Data)
-					{
-						Data data = (Data)object;
-						String method = key.substring(dot+1);
-						variable = data.get(method);
-					}
+				}
+				if (variable!=null){
+					break;
 				}
 			}
+			
 			if( isAlwaysReplace() && variable == null )
 			{
 				variable="";
@@ -113,6 +126,16 @@ public class Replacer
 		}
 		return inCode;
 	}
+	
+	protected ArrayList<String> findKeys(String Subject, String Delimiters) 
+    {
+		StringTokenizer tok = new StringTokenizer(Subject, Delimiters);
+		ArrayList<String> list = new ArrayList<String>(Subject.length());
+		while(tok.hasMoreTokens()){
+			list.add(tok.nextToken());
+		}
+		return list;
+    }
 
 	protected Data getData(String inType, String inId)
 	{
