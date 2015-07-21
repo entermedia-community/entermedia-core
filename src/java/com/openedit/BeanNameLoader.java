c35037b8697c7035af203b1ff4ac82f2f3e5df33
Christopher Burkey <cburkey@openedit.org>
2015-07-21 11:14:50 -0400

    Made default bean loaders

--- src/java/com/openedit/BeanNameLoader.java
+++ src/java/com/openedit/BeanNameLoader.java
@@ -1,24 +1,18 @@
 package com.openedit;
 
 
-import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
 import org.entermedia.cache.CacheManager;
 import org.openedit.xml.XmlArchive;
 import org.openedit.xml.XmlFile;
 
-import com.openedit.hittracker.HitTracker;
-import com.openedit.hittracker.SearchQuery;
 import com.openedit.page.Page;
-import com.openedit.page.PageSettings;
 import com.openedit.page.manage.PageManager;
-import com.openedit.util.PathUtilities;
 
 public class BeanNameLoader
 {
@@ -59,99 +53,22 @@ public class BeanNameLoader
 	
 	public String findName(String inCatalogId, String inId)
 	{
-		String cacheid = "beanLoader" + inCatalogId;
-		String  hit = (String)getCacheManager().get(cacheid, inId);
+		String cacheid =  inCatalogId + inId;
+		String  hit = (String)getCacheManager().get("beanNameloader", cacheid);
 		if (hit != null)
 		{
-			//return hit;
+			return hit;
 		}
-		String name = null;
-		
-//		XmlFile files = loadXmlFile(inCatalogId); //Never cached
-//		Element found = files.getElementById(inId);
-//		if( found != null)
-//		{
-//			name = found.attributeValue("bean");
-//		}
-//		if( name == null)
-//		{
-			name = legacyLoad(inCatalogId,inId);
-//		}
+		String name = legacyLoad(inCatalogId,inId);
 		if( name == null)
 		{
 			name = inId;
 		}
-		getCacheManager().put(cacheid,inId, name);
+		getCacheManager().put("beanNameloader",cacheid, name);
 		return name;
 	}
 	
-	protected XmlFile loadXmlFile(String inCatalogId)
-	{
-		try
-		{
-			String inName = "bean";
-			//getConfigurationPath
-			XmlFile composite = new XmlFile();
-			
-			String rootpath = "/WEB-INF/data/" + inCatalogId + "/lists/" + inName ;
-			composite.setPath(rootpath + "/custom.xml");
-
-			long inLastModified = getPageManager().getRepository().getStub(composite.getPath()).lastModified().getTime();
-			composite.setLastModified(inLastModified);
-			
-			Element root = DocumentHelper.createElement(inName);
-			composite.setRoot(root);
-
-			List<String> children = getPageManager().getChildrenPaths(rootpath,false);
-			if( children.size() > 0)
-			{
-				composite.setExist(true);
-				loadChildren(inName, children, root, true);
-			}
-			List<String> children2 = getPageManager().getChildrenPaths("/" + inCatalogId + "/data" + "/lists/" + inName + "/",true);
-			if( children2.size() > 0)
-			{
-				composite.setExist(true);
-				boolean existing = children.size() > 0;
-				loadChildren(inName, children2, root, true);
-			}
-//			HitTracker hits = getSearcherManager().getList(
-//					inCatalogId, "dataextensions");
-//			for (Iterator iterator = hits.iterator(); iterator
-//					.hasNext();) {
-//				Data hit = (Data) iterator.next();
-//				String catalogid = hit.get("catalogid");
-//
-//				List<String> children3 = getPageManager().getChildrenPaths("/" + catalogid + "/data" + "/lists/" + inName + "/",true);
-//				if( children3.size() > 0)
-//				{
-//					composite.setExist(true);
-//					boolean existing = children2.size() > 0;
-//					if( !existing )
-//					{
-//						existing = children.size() > 0;
-//					}
-//					loadChildren(inName, children3, root, existing);
-//				}
-//				
-//			}
-			
-			//remove deleted
-			for (Iterator iterator = new ArrayList(composite.getElements()).iterator(); iterator.hasNext();)
-			{
-				Element element = (Element) iterator.next();
-				if ( "deleted".equals( element.attributeValue("recordstatus") ) )
-				{
-					composite.getRoot().remove(element);
-				}
-			}
-			
-			return composite;
-		} catch ( OpenEditException ex)
-		{
-			throw new OpenEditRuntimeException(ex);
-		}
-	}
+	
 
 	protected void loadChildren(String inName, List<String> children, Element root, boolean noDups) 
 	{
@@ -200,16 +117,33 @@ public class BeanNameLoader
 		String parentlocation = "/" + inCatalogId + "/configuration/beans.xml";
 		Page page = getPageManager().getPage(parentlocation);
 		
-		if(page.exists())
+		if(!page.exists())
 		{
-			XmlFile file = getXmlArchive().getXml(page.getPath());
-			Element field = file.getElementById(inBeanName);
-			if( field != null)
+			page = getPageManager().getPage("/system/configuration/beans.xml");
+		}
+		
+		XmlFile file = getXmlArchive().getXml(page.getPath());
+		Element field = file.getElementById(inBeanName);
+		if( field != null)
+		{
+			//log.info("BEANS FOUND: " + field + ", with bean: " + field.attributeValue("bean"));
+			beanName = field.attributeValue("bean");
+		}
+		else
+		{
+			String type = file.getRoot().attributeValue("basedatatype");
+			if( type != null)
 			{
-				//log.info("BEANS FOUND: " + field + ", with bean: " + field.attributeValue("bean"));
-				beanName = field.attributeValue("bean");
+				file = getXmlArchive().getXml("/system/configuration/" + type + ".xml");
+				field = file.getElementById(inBeanName);
+				if( field != null)
+				{
+					//log.info("BEANS FOUND: " + field + ", with bean: " + field.attributeValue("bean"));
+					beanName = field.attributeValue("bean");
+				}
 			}
 		}
+		
 		return beanName;
 	}	
 	
