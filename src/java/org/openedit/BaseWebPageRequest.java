d6f190a056f843972fdb2a1da2e34849a1b5d230
Christopher Burkey <cburkey@openedit.org>
2016-09-14 11:18:48 -0400

    Custom date formats

--- src/java/org/openedit/BaseWebPageRequest.java
+++ src/java/org/openedit/BaseWebPageRequest.java
@@ -1258,17 +1258,25 @@ public class BaseWebPageRequest implements WebPageRequest, PageRequestKeys
 	
 	
 	
-	public String getDate(String inStorageFormat)
+	public String getDate(String inStoredDate)
 	{
-		return getLocaleManager().formatDateForDisplay( inStorageFormat, getLocale());
+		Date stored = getLocaleManager().getDateStorageUtil().parseFromStorage(inStoredDate);
+		return getDate(stored);
 	}
 	public String getDateTime(Date inDate)
 	{
+		String format = getUserProfileValue("datetimeformat");
+		if( format != null)
+		{
+			String value = getLocaleManager().getDateStorageUtil().formatDateObj(inDate, format);
+			return value;
+		}
 		return getLocaleManager().formatDateTimeForDisplay( inDate, getLocale());
 	}
-	public String getDateTime(String inStorageFormat)
+	public String getDateTime(String inStoredDate)
 	{
-		return getLocaleManager().formatDateTimeForDisplay( inStorageFormat, getLocale());
+		Date stored = getLocaleManager().getDateStorageUtil().parseFromStorage(inStoredDate);
+		return getDateTime(stored);
 	}
 	public String getText(String inKey)
 	{
