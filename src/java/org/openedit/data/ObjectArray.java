42ccb9b3b5368bf67a8285e9a47c2fee289a7d62
Christopher Burkey <cburkey@openedit.org>
2017-05-02 16:36:44 -0400

    Object array

---
+++ src/java/org/openedit/data/ObjectArray.java
@@ -0,0 +1,38 @@
+package org.openedit.data;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Map;
+
+
+
+public class ObjectArray
+{
+	protected Collection<ValuesMap> fieldValueMaps;
+	
+	public ObjectArray(Collection inGenericMaps)
+	{
+		for (Iterator iterator = inGenericMaps.iterator(); iterator.hasNext();)
+		{
+			Map object = (Map) iterator.next();
+			getMaps().add(new ValuesMap(object));
+		}
+	}
+
+	public Collection<ValuesMap> getMaps()
+	{
+		if (fieldValueMaps == null)
+		{
+			fieldValueMaps = new ArrayList<>();
+		}
+		return fieldValueMaps;
+	}
+
+	public void setMaps(Collection<ValuesMap> inMaps)
+	{
+		fieldValueMaps = inMaps;
+	}
+	
+	
+}
