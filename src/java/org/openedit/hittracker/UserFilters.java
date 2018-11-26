c9393707f9167825cefc286c279bb65af5f55583
Ian <ian@netevolved.com>
2018-11-26 15:46:25 -0500

    More filtering code

--- src/java/org/openedit/hittracker/UserFilters.java
+++ src/java/org/openedit/hittracker/UserFilters.java
@@ -12,5 +12,6 @@ public interface UserFilters
 	public void setUserProfile(UserProfile inUserProfile);
 	public List<FilterNode> getFilterOptions(String inSearchType, SearchQuery inSearchQuery);
 	public void clear(String inSearchType) ;
+	public void addFilterOptions(String inSearchType, SearchQuery inQuery, List<FilterNode> inFilters);
 
 }
