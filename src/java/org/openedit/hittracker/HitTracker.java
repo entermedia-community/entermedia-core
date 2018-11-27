b4a1427cd3c685d307bb9ea9199e4ec4fdbdf39b
cburkey <cburkey@entermediadb.org>
2018-11-27 14:46:33 -0500

    Inputs

--- src/java/org/openedit/hittracker/HitTracker.java
+++ src/java/org/openedit/hittracker/HitTracker.java
@@ -1412,8 +1412,8 @@ public abstract class HitTracker<T> implements Serializable, Collection, Catalog
 	
 	public boolean isInputEquals(String inDetail,String inValue)
 	{
-		String input = getSearchQuery().getInput(inDetail);
-		if( inValue.equals(input))
+		Collection values = getSearchQuery().getInputs(inDetail);
+		if( values.contains(inValue))
 		{
 			return true;
 		}
