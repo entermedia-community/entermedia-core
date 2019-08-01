0e1134b45e9d863503aad7e813dd42b3f876e447
cburkey <cburkey@entermediadb.org>
2019-08-01 12:33:18 -0400

    Added init option

--- src/java/org/openedit/data/SearcherManager.java
+++ src/java/org/openedit/data/SearcherManager.java
@@ -45,9 +45,14 @@ public class SearcherManager
 		fieldCacheManager = inCacheManager;
 	}
 
+	public Searcher getSearcher(String inCatalogId, String inSearchtypeid)
+	{
+		Searcher searcher = loadSearcher(inCatalogId,inSearchtypeid, false);
+		return searcher;
+	}
 
 	//A fieldName can be product or orderstatus. If there is no orderstatus searcher then we use an XML lookup for this catalog. 
-	public Searcher getSearcher(String inCatalogId, String inFieldName)
+	public Searcher loadSearcher(String inCatalogId, String inFieldName,boolean init)
 	{
 		if( inCatalogId == null)
 		{
@@ -78,22 +83,19 @@ public class SearcherManager
 							return searcher;
 						}
 					}
-					//TODO: Look in the cache again for the target Searcher
 					boolean created = getNodeManager(finalcatalogid).connectCatalog(finalcatalogid);
 					
 					PropertyDetailsArchive newarchive = getPropertyDetailsArchive(finalcatalogid);
-//					if( inFieldName == null)
-//					{
-//						return null;
-//					}
-					
 					searcher = loadSearcher(newarchive, inFieldName);
 					searcher.setCatalogId(finalcatalogid);
 					searcher.setSearchType(inFieldName); //This may be product or orderstatus
 					//set the data
 					searcher.setPropertyDetailsArchive(newarchive);
 					searcher.setSearcherManager(this);
-					searcher.initialize();
+					if( init )
+					{
+						searcher.initialize();
+					}
 					if(log.isDebugEnabled())
 					{
 						log.debug("Created New Searcher: Catalog = " + searcher.getCatalogId() + "SearchType = " + searcher.getSearchType() + "Searcher = " + searcher.getClass() );
@@ -664,6 +666,7 @@ public class SearcherManager
 			
 		}
 	}
+
 	
 	
 }
