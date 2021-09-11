4dcf8a8f09733fb66acbfa3e63dbd8dc17967fcc
Christopher Burkey <cburkey@entermediadb.org>
2021-09-11 12:12:31 -0600

    Speed up translation

--- src/java/org/openedit/modules/translations/Translation.java
+++ src/java/org/openedit/modules/translations/Translation.java
@@ -20,7 +20,9 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.openedit.Data;
@@ -156,7 +158,22 @@ public class Translation
 	{
 		return webTranslate(text,"en",inLocale);
 	}
+	
 	public String webTranslate(String text, String sourcelang, String inLocale)
+	{
+		try
+		{
+			HttpClient client = URLUtilities.createTrustingHttpClient().setUserAgent("Mozilla/5.0 (Mobile; rv:14.0) Gecko/14.0 Firefox/14.0").build();
+			String response = webTranslate(client,text,sourcelang, inLocale);
+			return response;
+		} catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public String webTranslate(HttpClient client,String text, String sourcelang, String inLocale)
 	{
 		text.replace("$", "_");
 		if( inLocale.equals("de"))
@@ -172,8 +189,10 @@ public class Translation
 
 		try
 		{
-		HttpClient client = URLUtilities.createTrustingHttpClient().setUserAgent("Mozilla/5.0 (Mobile; rv:14.0) Gecko/14.0 Firefox/14.0").build();
 		HttpPost httpPost = new HttpPost( googlePage );
+		
+		//post.setEntity(new StringEntity(inBody.toString(), "UTF-8"));
+		
 		List <NameValuePair> nvps = new ArrayList <NameValuePair>();
 		
 		
@@ -185,6 +204,7 @@ public class Translation
 		nvps.add(new BasicNameValuePair("q", text));
 		nvps.add(new BasicNameValuePair("source", sourcelang));
 		nvps.add(new BasicNameValuePair("target", inLocale));
+//		nvps.add(new BasicNameValuePair("format", "text"));
 //		nvps.add(new BasicNameValuePair("client", "t"));
 //		nvps.add(new BasicNameValuePair("text", text));
 //		nvps.add(new BasicNameValuePair("hl", "en"));
@@ -203,13 +223,12 @@ public class Translation
 //			e1.printStackTrace();
 //		}
 		httpPost.setHeader("X-HTTP-Method-Override","GET");
-		httpPost.setEntity(new UrlEncodedFormEntity(nvps));
+		httpPost.setEntity(new UrlEncodedFormEntity(nvps,"UTF-8"));
 			HttpResponse response2 = client.execute(httpPost);
 		
 		    if(response2.getStatusLine().getStatusCode() != -1 ) 
 		    {
 		    	HttpEntity entity2 = response2.getEntity();
-		    	String charset = "UTF-8";
 //		    	Header[] headers = response2.getHeaders("Content-Type");
 //		    	if (headers!=null && headers.length > 0)
 //		    	{
@@ -226,7 +245,12 @@ public class Translation
 //		    	}
 		    	//log.info(EntityUtils.toString(entity2));
 		    	//StringWriter out = new StringWriter();
-		    	JSONObject config = (JSONObject)new JSONParser().parse(new InputStreamReader( entity2.getContent(), charset ));
+		    	//String encodedas = entity2.getContentEncoding().getValue();
+		    	
+		    	String returned = EntityUtils.toString(entity2);
+		    	
+		    	//JSONObject config = (JSONObject)new JSONParser().parse(new InputStreamReader( entity2.getContent(), charset ));
+		    	JSONObject config = (JSONObject)new JSONParser().parse(returned);
 		    	JSONObject data = (JSONObject)config.get("data");
 		    	if( data != null)
 		    	{
