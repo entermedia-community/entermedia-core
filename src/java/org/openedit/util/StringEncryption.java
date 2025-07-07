cda6f1ebd5feeeffeca5c3d04523b712350441a7
Ian <ian@oneliveweb.com>
2025-07-07 11:43:27 -0600

    New algo - needed for FB

--- src/java/org/openedit/util/StringEncryption.java
+++ src/java/org/openedit/util/StringEncryption.java
@@ -552,4 +552,25 @@ public class StringEncryption
 	    }
 	}
 	
+	
+	public String getSha256(String input) {
+		try {
+			MessageDigest digest = MessageDigest.getInstance("SHA-256");
+			byte[] encodedHash = digest.digest(input.getBytes(UTF8_CHARSET));
+
+			StringBuilder hexString = new StringBuilder();
+			for (byte b : encodedHash) {
+				String hex = Integer.toHexString(0xff & b);
+				if (hex.length() == 1) hexString.append('0');
+				hexString.append(hex);
+			}
+			return hexString.toString();
+		} catch (NoSuchAlgorithmException e) {
+			throw new OpenEditException("SHA-256 hashing failed: " + e.getMessage(), e);
+		}
+	}
+	
+	
+	
+	
 }
