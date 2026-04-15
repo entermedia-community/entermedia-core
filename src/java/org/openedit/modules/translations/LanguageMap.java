ade876344b918b1f1267720717c446c1f1958cbc
Cristobal <cristobal.mejia@gmail.com>
2026-04-15 13:04:37 -0600

    Language Map

--- src/java/org/openedit/modules/translations/LanguageMap.java
+++ src/java/org/openedit/modules/translations/LanguageMap.java
@@ -11,39 +11,36 @@ import org.json.simple.JSONObject;
 public class LanguageMap extends TreeMap
 {
 
-	public LanguageMap(Map inVals)
-	{
+	public LanguageMap(Map inVals) {
 		for (Iterator iterator = inVals.keySet().iterator(); iterator.hasNext();)
 		{
 			String key = (String) iterator.next();
-			String value = (String)inVals.get(key);
-			if( value == null || value.trim().isEmpty() )
+			String value = (String) inVals.get(key);
+			if (value == null || value.trim().isEmpty())
 			{
 				continue;
 			}
-			put(key,value);
+			put(key , value);
 		}
 	}
-	
-	public LanguageMap()
-	{
-	}
+
+	public LanguageMap() {}
 
 	public String getText(String inLocale)
 	{
-		if( inLocale == null)
+		if (inLocale == null)
 		{
 			return getText("en");
 		}
 		String val = (String) get(inLocale);
-		if(val == null && inLocale.contains("_"))
+		if (val == null && inLocale.contains("_"))
 		{
 			String[] splits = inLocale.split("_");
-			if(splits.length == 3)
+			if (splits.length == 3)
 			{
-				val = (String) get(splits[0]+ "_" + splits[1]);
+				val = (String) get(splits[0] + "_" + splits[1]);
 			}
-			if(val == null && splits.length == 2)
+			if (val == null && splits.length == 2)
 			{
 				val = (String) get(splits[0]);
 			}
@@ -54,65 +51,68 @@ public class LanguageMap extends TreeMap
 	public String getDefaultText(String inLocale)
 	{
 		String val = getText(inLocale);
-		if( val == null && inLocale != null && !inLocale.equals("en") )
+		if (val == null && inLocale != null && !inLocale.equals("en"))
 		{
 			val = getText("en");
 		}
 		return val;
 	}
+
 	public void setText(String inLocale, String inVal)
 	{
-		if( inVal == null || inVal.isEmpty())
+		if (inVal == null || inVal.isEmpty())
 		{
 			remove(inLocale);
 		}
 		else
 		{
-			put(inLocale, inVal);
+			put(inLocale , inVal);
 		}
 	}
-	
+
 	@Override
 	public String toString()
 	{
-		if( isEmpty())
+		if (isEmpty())
 		{
 			return null;
 		}
-		if( size() == 1)
+		if (size() == 1)
 		{
-			return (String)values().iterator().next();
+			return (String) values().iterator().next();
 		}
 		String json = toJson();
-		if(json != null){
+		if (json != null)
+		{
 			return json;
-		} 
-		return "";		
+		}
+		return "";
 	}
 
 	public String toJson()
 	{
-		if( isEmpty())
+		if (isEmpty())
 		{
 			return null;
 		}
 		JSONObject json = new JSONObject();
-		json.putAll(this);	
+		json.putAll(this);
 		return json.toJSONString();
 	}
-	
+
 	@Override
-	public boolean equals(Object inO) {
-		if( inO instanceof LanguageMap)
+	public boolean equals(Object inO)
+	{
+		if (inO instanceof LanguageMap)
 		{
-			LanguageMap map = (LanguageMap)inO;
-			if(map.size() != size())
+			LanguageMap map = (LanguageMap) inO;
+			if (map.size() != size())
 			{
 				return false;
 			}
 			Collection copy = new ArrayList(values());
 			copy.removeAll(map.values());
-			if( !copy.isEmpty())
+			if (!copy.isEmpty())
 			{
 				return false;
 			}
@@ -124,19 +124,18 @@ public class LanguageMap extends TreeMap
 	@Override
 	public boolean isEmpty()
 	{
-	
+
 		for (Iterator iterator = keySet().iterator(); iterator.hasNext();)
 		{
 			String key = (String) iterator.next();
 			Object val = get(key);
-			if(val != null){
+			if (val != null)
+			{
 				return false;
 			}
 		}
-		
+
 		return true;
 	}
-	
-	
-	
+
 }
